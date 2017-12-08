55ad3ce59d2175ff8276aee53629084b7e192fa4
Alessandro Botelho <comoltecnologia@gmail.com>
2017-12-08 09:30:31 -0200

    Criamos a classe usuario

--- class/sql.php
+++ class/sql.php
@@ -7,13 +7,13 @@ class sql extends PDO{
         $this->conn = new PDO("mysql:host=localhost;dbname=dbphp7","root","comol123");
     }
     
-    private function setParams($statment, $parameters= array()){
+    private function setParams($statement, $parameters= array()){
         foreach ($parameters as $key => $value) {
-            $this->setParam($key, $value);
+            $this->setParam($statement,$key, $value);
         }
     }
-    private function setParam($statmet ,$key, $value){
-        $statmet->bindParam($key,$value);
+    private function setParam($statement ,$key, $value){
+        $statement->bindParam($key,$value);
     }
 
         public function query($rawQuery, $params = array()){
--- index.php
+++ index.php
@@ -1,10 +1,17 @@
 <?php
   require_once ("config.php");
   
+  $alessandro = new Usuario();
+  $alessandro->loadById(1);
+  
+  echo $alessandro;
+  
+  
+/*  
+ * retorna todos os cadastros
   $sql = new sql();
-  
   $usuario = $sql->select("SELECT * FROM tb_usuario");
-  
-  echo json_encode($usuario);
+  echo json_encode($usuario); 
+ */
 ?>
 
